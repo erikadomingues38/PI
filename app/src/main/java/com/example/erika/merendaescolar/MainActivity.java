7e8be176cd403b0f44046c12484535ae08a0f09d
erikadomingues38 <erika.domingues38@gmail.com>
2015-12-01 21:34:00 -0200

    test 2

---
+++ app/src/main/java/com/example/erika/merendaescolar/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.erika.merendaescolar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by erika on 13/10/2015.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
